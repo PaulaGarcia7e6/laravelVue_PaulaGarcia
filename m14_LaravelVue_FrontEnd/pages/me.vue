6d590c91242a6df8fd03a90f5caf8034f57d149c
Paula <paula.garcia.7e6@itb.cat>
2024-02-06 18:35:40 +0100

    update

---
+++ m14_LaravelVue_FrontEnd/pages/me.vue
@@ -0,0 +1,27 @@
+<template>
+  <p><strong>User Name:</strong> {{user.name}}</p>
+  <p><strong>User Email:</strong> {{user.email}}</p>
+</template>
+<script script lang="ts">
+import axios from 'axios';
+interface User {
+  "name": string,
+  "email": string,
+}
+let user:User = reactive({
+  name:"",
+  email:""
+});
+
+async function userData() {
+  try {
+    user = await axios.get('/user' )
+    console.log(user)
+  } catch(error){
+    console.log(error)
+  }
+}
+onBeforeMount(async()=>{
+  await userData()
+})
+</script>
